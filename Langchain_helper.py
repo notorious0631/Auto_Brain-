5e8a2acef8c2e2b206d46d5e54c96eaa2a56da7b
notorious0631 <dsonu0631@gmail.com>
2025-10-30 02:01:01 +0530

    Update Langchain_helper.py

--- Langchain_helper.py
+++ Langchain_helper.py
@@ -1,4 +1,4 @@
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import CSVLoader
 from langchain_classic.prompts import PromptTemplate
